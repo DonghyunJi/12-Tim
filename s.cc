ccc75d8e56789d8d93a704426d8219d4b35d5070
DonghyunJi <dhchi0827@gmail.com>
2020-12-10 17:33:15 +0900

    add comparator and SAR_logic

--- s.cc
+++ s.cc
@@ -1,42 +1,40 @@
 #include<cstdio>
+#include<cstdlib>
 #include<iostream>
+#include<queue>
 
 using namespace std;
 
 class Output {
-public:
-	queue<int>data;	
-	double input;	
-	int check;	
+	public:
+			queue<int>data;	
+				double input;	
+					int check;		
 };
 
-double DAC(Output output) {
-	queue<int>sample;
-	int i, j;
-	double x;
-	double ref = 0;
+DAC(){}
 
-	sample = output.data;
-
-	for (i = 0; i < output.check + 1; i++) {
-		x = 1;
-		for (j = 0; j < i + 1; j++) x = x * 1 / 2;
-		if (i != output.check) {
-			ref = ref + 10 * x * sample.front();
-			sample.pop();
-		}
-		else ref = ref + 10 * x;
-	}
-	cout << "Input Voltage : " << output.input << " V" << " / Reference Voltage : " << ref << " V" << endl;
-	return ref;
+bool comparator(double a, double b) {
+		return a >= b;
 }
 
-bool comparator(){}
+Output SAR_logic(Output output) {
+		queue<int>sample;
+			int i;
 
-SAR_logic(){}
+				output.data.push(Comparator(output.input, DAC(output)));
+					output.check++;
+						sample = output.data;
+							cout << output.check << "'s cycle : ";
+								for (i = 0; i < output.check; i++) {
+											cout << sample.front() << " ";
+													sample.pop();
+														}
+									cout << endl;
+										return output;
+}
 
 int main(){
 
 }
-
 
